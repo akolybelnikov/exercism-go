4669b7d687ac8ea7b39c4d6a1c1c7234ab5b5d4e
Andrey Kolybelnikov <a.kolybelnikov@gmail.com>
2021-05-03 17:08:41 +0200

    Allergies

--- allergies/allergies.go
+++ allergies/allergies.go
@@ -1,1 +1,39 @@
+// Package allergies implements a solution to the problem on the Go track of Exercism
 package allergies
+
+var substances = []string{
+	"eggs",
+	"peanuts",
+	"shellfish",
+	"strawberries",
+	"tomatoes",
+	"chocolate",
+	"pollen",
+	"cats",
+}
+
+// Allergies determines the full list of allergies given the person's score.
+func Allergies(score uint) (results []string) {
+	for k, v := range substances {
+		// calculate the allergy score by shifting the bits
+		r := uint(1 << k)
+		if r&score == r {
+			results = append(results, v)
+		}
+	}
+	return
+}
+
+// AllergicTo determines whether or not the person is allergic to a given item given their score.
+func AllergicTo(score uint, substance string) bool {
+	for k, v := range substances {
+		if v == substance {
+			// calculate the allergy score by shifting the bits
+			r := uint(1 << k)
+			if r&score == r {
+				return true
+			}
+		}
+	}
+	return false
+}
