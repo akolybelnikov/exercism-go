9e5d094caaa82259b54407bca5555c07460ccb02
Andrey Kolybelnikov <a.kolybelnikov@gmail.com>
2021-05-03 10:41:53 +0200

    Parallel letter frequency v2

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,7 +1,18 @@
+// Package letter implements a solution to the problem on Exercism
 package letter
+
+import (
+	"sync"
+)
 
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
+
+type safeFrequency struct {
+	mu sync.Mutex
+	fm FreqMap
+	wg sync.WaitGroup
+}
 
 // Frequency counts the frequency of each rune in a given text and returns this
 // data as a FreqMap.
@@ -11,4 +22,25 @@ func Frequency(s string) FreqMap {
 		m[r]++
 	}
 	return m
+}
+
+// ConcurrentFrequency counts the frequency of each rune in each text in a list of texts concurrently
+// and returns this data as a FreqMap.
+func ConcurrentFrequency(texts []string) FreqMap {
+	sf := safeFrequency{fm: make(FreqMap)}
+	for _, t := range texts {
+		sf.wg.Add(1)
+		go sf.concFrequency(t)
+	}
+	sf.wg.Wait()
+	return sf.fm
+}
+
+func (f *safeFrequency) concFrequency(s string) {
+	defer f.wg.Done()
+	for _, r := range s {
+		f.mu.Lock()
+		f.fm[r]++
+		f.mu.Unlock()
+	}
 }
